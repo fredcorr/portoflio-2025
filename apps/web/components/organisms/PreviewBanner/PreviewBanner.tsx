4be6a0186f6385e51d6e8056ea623b8dcfb71862
Federico Corradi <itsos.corradi@gmail.com>
2026-02-10 17:46:28 +0100

    fix: preview banner position

--- apps/web/components/organisms/PreviewBanner/PreviewBanner.tsx
+++ apps/web/components/organisms/PreviewBanner/PreviewBanner.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const PreviewBanner = () => {
   return (
-    <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 mb-4">
+    <div className="fixed bottom-4 left-1/2 z-50 w-[calc(100%-2rem)] max-w-[520px] -translate-x-1/2 rounded-lg border-l-4 border-yellow-500 bg-yellow-100 p-4 text-yellow-700 shadow-lg">
       <p className="font-bold">Draft Mode</p>
       <p>
         You are viewing draft content.{' '}
